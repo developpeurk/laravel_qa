3e005fae76e7b7edee626f597e2409735cb7abd3
Yassine LAMBARKI <TechCmr@AISYTEK.COM>
2019-08-30 08:54:18 +0100

    Implement validation and store

--- laravel_qa/app/Http/Controllers/QuestionsController.php
+++ laravel_qa/app/Http/Controllers/QuestionsController.php
@@ -41,7 +41,9 @@ class QuestionsController extends Controller
      */
     public function store(AskQuestionRequest $request)
     {
-      dd('store');
+        $request->user()->questions()->create($request->only('title', 'body'));
+
+        return redirect()->route('questions.index')->with('success', 'your question has been submitted');
     }
 
     /**
